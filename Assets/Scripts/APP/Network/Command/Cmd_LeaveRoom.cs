943e7f9121fe952a6c363da54521f27fcdb02ac2
xpy <peiying0310@163.com>
2026-04-21 23:14:41 +0900

    feat(net): 离开房间后清除 LastRoomCode

--- Assets/Scripts/APP/Network/Command/Cmd_LeaveRoom.cs
+++ Assets/Scripts/APP/Network/Command/Cmd_LeaveRoom.cs
@@ -3,6 +3,7 @@ using APP.Network.DTO;
 using APP.Network.Event;
 using APP.Network.Model;
 using APP.Network.System;
+using APP.SessionMemory.Model;
 using QFramework;
 
 namespace APP.Network.Command
@@ -12,14 +13,13 @@ namespace APP.Network.Command
         protected override void OnExecute()
         {
             IRoomModel room = this.GetModel<IRoomModel>();
-            this.GetSystem<INetworkSystem>().Send(new OutboundLeaveRoom
-            {
-                type = "leave_room",
-            });
+            this.GetSystem<INetworkSystem>().Send(new OutboundLeaveRoom { type = "leave_room" });
 
             room.ResetRoomState();
             this.SendEvent(new E_RoomSnapshot(new List<RemotePlayerData>()));
             room.SetStatus(room.IsConnected.Value ? ConnectionStatus.Connected : ConnectionStatus.Disconnected);
+
+            this.GetModel<ISessionMemoryModel>().ForgetLastRoom();
         }
     }
 }
